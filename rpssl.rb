08c4be0e2370872534131c2e6f907355fb76ac56
Leon Hu <tihuinc@gmail.com>
2010-04-07 12:02:31 -0700

    Creates helper method to simplifies the views for index.

--- rpssl.rb
+++ rpssl.rb
@@ -4,6 +4,22 @@ require 'haml'
 
 require 'lib/game'
 
+helpers do
+  def item_image(user_choice, computer_choice, item)
+    if user_choice && user_choice.to_s == item
+      src_image = "#{item}_user"
+      "<img id='#{item}_image' src='images/#{item}_user.png' />"
+    elsif computer_choice && computer_choice.to_s == item
+      src_image = "#{item}_computer"
+    else
+      src_image = "#{item}"
+    end
+
+    "<img id='#{item}_image' src='images/#{src_image}.png' />"
+  end
+end
+
+
 get '/' do
    haml :index, :locals => { :user_choice => nil, :computer_choice => nil,
       :result => nil }
